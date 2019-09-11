a9c9046c15014dfeea4fae5d2a531565124e3b8f
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2019-09-10 23:48:46 -0400

    generating data and displaying it

--- demo/src/specmonstah_demo/components/vendor/ace.cljs
+++ demo/src/specmonstah_demo/components/vendor/ace.cljs
@@ -11,9 +11,20 @@
       (dissoc :type)))
 
 (defmethod stfc/input :ace
-  [{:keys [partial-form-path attr-path value]}]
+  [{:keys [partial-form-path attr-path value height width]}]
   [:> AceEditor {:onChange (fn [val] (stfc/dispatch-change partial-form-path attr-path val))
                  :name     "ACE BABY"
                  :value    (or value "")
+                 :height   (or height "300px")
+                 :width    (or width "300px")
                  :theme    "monokai"
                  :mode     "clojure"}])
+
+(defn ace-readonly
+  [text opts]
+  [:> AceEditor (merge {:name     "ace readonly"
+                        :value    (or text "")
+                        :theme    "monokai"
+                        :mode     "clojure"
+                        :readOnly true}
+                       opts)])
